56213428b36b299e7c822efdce3ad1c65f0523d6
Khang Nguyen <62628865+khangaerospace@users.noreply.github.com>
2023-01-07 12:02:41 -0500

    Create Example.py

---
+++ Hack6/Example.py
@@ -0,0 +1,15 @@
+"""This is a simple math code to demonstrate how to do a pull request on Github"""
+
+
+def add(a, b):
+    sum = a + b
+    return sum
+
+
+if __name__ == "__main__":
+    a = 3
+    b = 5
+    sum = add(a, b)
+    print(sum)
+    # let's also print Hello World
+    print("Hello world")
